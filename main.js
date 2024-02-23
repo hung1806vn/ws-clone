3b5d1ed652c5f7ae13dbd1fc3c7754cf42828965
Hung Vu <hung.vu@axon-networks.com>
2024-02-23 13:19:09 +0700

    implement chating with my love

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 // Importing the required modules
 const WebSocketServer = require('ws');
- 
+const readline = require('node:readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
 // Creating a new websocket server
 const wss = new WebSocketServer.Server({ port: 8080 })
- 
+
 // Creating connection using websocket
 wss.on("connection", ws => {
     console.log("new client connected");
- 
+
     // sending message to client
     ws.send('Welcome, you are connected!');
- 
+
+    var waitForUserInput = function () {
+        readline.question("Gửi message đến client: \n", function (message) {
+            ws.send(message);
+            if (message == "exit") {
+                readline.close();
+            } else {
+                waitForUserInput();
+            }
+        });
+    }
+
+    waitForUserInput();
+
     //on message from client
     ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`)
-        // sending message to client
-        ws.send('Yêu em nhiều tục tưng!');
+        console.log(`Client has sent us: ${data} \n`)
     });
- 
+
     // handling what to do when clients disconnects from server
     ws.on("close", () => {
         console.log("the client has connected");
@@ -28,3 +43,4 @@ wss.on("connection", ws => {
     }
 });
 console.log("The WebSocket server is running on port 8080");
+
